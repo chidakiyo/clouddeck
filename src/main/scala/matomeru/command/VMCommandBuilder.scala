00fdd3168e9c10dee4ba824ddd43c19b131ebae3
kiyo <kiyokatsu.chida@weserve.co.jp>
2014-08-08 16:17:37 +0900

    implement vmware-cli wrapper.

--- src/main/scala/matomeru/command/VMCommandBuilder.scala
+++ src/main/scala/matomeru/command/VMCommandBuilder.scala
@@ -3,16 +3,145 @@ package matomeru.command
 object VMCommandBuilder {
 
   val dq = """""""
+  val vc = "vmware-cmd"
+  val vh = "vicfg-hostops"
+  val fs = "vifs"
+  val es = "esxcli"
 
+  // vicfg-hostops
+
+  /** ESXi host information */
   def hostInfo()(implicit info: ConnectInfo): String = {
-    s"vicfg-hostops --server ${info.host} --username ${info.user} --password ${info.pass} --operation info"
+    s"${vhBase(info)} --operation info"
   }
 
+  /** maintenance mode start */
+  def hostMaintenanceOn()(implicit info: ConnectInfo): String = {
+    s"${vhBase(info)} --operation enter"
+  }
+
+  /** maintenance mode end */
+  def hostMaintenanceOff()(implicit info: ConnectInfo): String = {
+    s"${vhBase(info)} --operation exit"
+  }
+
+  /** ESXi host reboot */
+  def hostReboot()(implicit info: ConnectInfo): String = {
+    s"${vhBase(info)} --operation reboot"
+  }
+
+  /** ESXi host shutdown */
+  def hostShutdown()(implicit info: ConnectInfo): String = {
+    s"${vhBase(info)} --operation shutdown"
+  }
+
+  // vmware-cmd
+
+  /** get guest os list */
   def guestList()(implicit info: ConnectInfo): String = {
-    s"vmware-cmd --server ${info.host} --username ${info.user} --password ${info.pass} -l"
+    s"${vcBase(info)} -l"
   }
 
-  def guestState(line: String)(implicit info: ConnectInfo): String = {
-    s"vmware-cmd --server ${info.host} --username ${info.user} --password ${info.pass} $dq$line$dq getstate"
+  /** guest os power status */
+  def guestState(path: String)(implicit info: ConnectInfo): String = {
+    s"${vcBase(info)} $dq$path$dq getstate"
+  }
+
+  /** guest os power on */
+  def guestPowerOn(path: String)(implicit info: ConnectInfo): String = {
+    s"${vcBase(info)} $dq$path$dq start"
+  }
+
+  /** guest os power off (require VMWare Tools installed) */
+  def guestPowerOff(path: String)(implicit info: ConnectInfo): String = {
+    s"${vcBase(info)} $dq$path$dq stop soft"
+  }
+
+  /** guest os power off hard */
+  def guestPowerOffHard(path: String)(implicit info: ConnectInfo): String = {
+    s"${vcBase(info)} $dq$path$dq stop hard"
+  }
+
+  /** register to inventory */
+  @deprecated("untested", "0.0.0")
+  def guestRegisterInventory(path: String)(implicit info: ConnectInfo): String = {
+    s"${vcBase(info)} -s register $dq$path$dq"
+  }
+
+  /** remove from inventory */
+  @deprecated("untested", "0.0.0")
+  def guestRemoveInventory(path: String)(implicit info: ConnectInfo): String = {
+    s"${vcBase(info)} -s unregister $dq$path$dq"
+  }
+
+  // vifs
+
+  /** Put file to Datastore */
+  def fsPut(localPath: String, datastorePath: String)(implicit info: ConnectInfo): String = {
+    s"${fsBase(info)} -p $dq$localPath$dq $dq$datastorePath$dq"
+  }
+
+  /** Get file from Datastore */
+  def fsGet(localPath: String, datastorePath: String)(implicit info: ConnectInfo): String = {
+    s"${fsBase(info)} -g $dq$datastorePath$dq $dq$localPath$dq"
+  }
+
+  /** Copy file DS1 to DS2 */
+  def fsCopy(srcDSPath: String, dstDSPath: String)(implicit info: ConnectInfo): String = {
+    s"${fsBase(info)} -c $dq$srcDSPath$dq $dq$dstDSPath$dq"
+  }
+
+  /** Move file DS1 to DS2 */
+  def fsMove(srcDSPath: String, dstDSPath: String)(implicit info: ConnectInfo): String = {
+    s"${fsBase(info)} -m $dq$srcDSPath$dq $dq$dstDSPath$dq"
+  }
+
+  /** Remove file */
+  def fsRemove(datastorePath: String)(implicit info: ConnectInfo): String = {
+    s"${fsBase(info)} -r $dq$datastorePath$dq"
+  }
+
+  /** Show Datastore list */
+  def fsList()(implicit info: ConnectInfo): String = {
+    s"${fsBase(info)} -S"
+  }
+
+  // esxcli
+
+  /** find applied patch */
+  def checkPatchList()(implicit info: ConnectInfo): String = {
+    s"${esBase(info)} software vib list"
+  }
+
+  /** check patch info */
+  def checkPatchInfo(patchPath: String)(implicit info: ConnectInfo): String = {
+    s"${esBase(info)} software sources vib list -d $dq$patchPath$dq"
+  }
+
+  /** apply patch */
+  def applyPatch(patchPath: String)(implicit info: ConnectInfo): String = {
+    s"${esBase(info)} software vib update -d $dq$patchPath$dq"
+  }
+
+  // base
+
+  private def vcBase(info: ConnectInfo): String = {
+    s"$vc ${base(info)}"
+  }
+
+  private def vhBase(info: ConnectInfo): String = {
+    s"$vh ${base(info)}"
+  }
+
+  private def fsBase(info: ConnectInfo): String = {
+    s"$fs ${base(info)}"
+  }
+
+  private def esBase(info: ConnectInfo): String = {
+    s"$es ${base(info)}"
+  }
+
+  private def base(info: ConnectInfo): String = {
+    s"--server ${info.host} --username ${info.user} --password ${info.pass}"
   }
 }
