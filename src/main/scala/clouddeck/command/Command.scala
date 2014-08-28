58b0bac3870800acc56d2ff9dadafed8dc548fae
kiyo <kiyokatsu.chida@weserve.co.jp>
2014-08-28 14:09:14 +0900

    Change of command string for execute environment

--- src/main/scala/clouddeck/command/Command.scala
+++ src/main/scala/clouddeck/command/Command.scala
@@ -1,9 +1,27 @@
 package clouddeck.command
 
-/**
- * Execute external command
- */
-object Command {
+import clouddeck.util.OSUtil
+import clouddeck.Keys.Option._
 
+/**
+ *
+ */
+class Command(cmd: String) {
+  val isWindows = OSUtil.isWindows()
+
+  def cmdStr() = {
+    isWindows match {
+      case true => s"cmd.exe /c ${cmd}.pl"
+      case false => cmd
+    }
+  }
+
+  def cmdAndOpt(info: ConnectInfo): String = {
+    this.cmdStr + basicOption(info)
+  }
+}
+
+object Command {
+  def apply(cmd: String): Command = new Command(cmd)
 }
 
