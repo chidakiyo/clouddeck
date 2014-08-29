78acff456bdc7896f4b9d4a249ef4a46a42fcef1
kiyo <kiyokatsu.chida@weserve.co.jp>
2014-08-29 11:11:33 +0900

    Removal of not using function

--- src/main/scala/clouddeck/implicits/Implicits.scala
+++ src/main/scala/clouddeck/implicits/Implicits.scala
@@ -1,8 +1,4 @@
 package clouddeck.implicits
 
 object Implicits {
-  implicit class CommandString(val com: String) extends AnyVal {
-    import scala.sys.process._
-    def !!!(): String = com.!!
-  }
 }
