92a18981e2d56bd9813935e35171e245579bbdef
yukinagae <yuki.nagae1130@gmail.com>
2014-09-05 14:10:17 +0900

    command execute in guest os using VIX (supposedly use `curl` with POST forms and get client info from parameters)

--- src/main/scala/clouddeck/command/VIXCommandBuilder.scala
+++ src/main/scala/clouddeck/command/VIXCommandBuilder.scala
@@ -2,6 +2,7 @@ package clouddeck.command
 
 import clouddeck.command.Commands._
 import clouddeck.Keys.Const._
+import java.net.InetAddress
 
 object VIXCommandBuilder {
 
@@ -13,6 +14,20 @@ object VIXCommandBuilder {
   /** guest os power off */
   def guestPowerOff(path: String)(implicit info: ConnectInfo): String = {
     s"${VIX_CMD.cmdAndOpt_vix(info)} stop ${quot(path)}"
+  }
+
+  /** run command in guest os */
+  def runInGuest(path: String, command: String)(user: String, pass: String)(implicit info: ConnectInfo): String = {
+    s"${VIX_CMD.cmdAndOpt_vix(info)} -gu ${user} -gp ${pass} runProgramInGuest ${quot(path)} ${command}"
+  }
+
+  val CURL_PATH = "/usr/bin/curl"
+  val localAddress = InetAddress.getLocalHost().getHostAddress()
+
+  def curl(params: Map[String, String]): String = {
+    import clouddeck.AppConfig
+    val param = params.foldLeft("") { (s: String, pair: (String, String)) => s + " -d " + pair._1 + "=" + pair._2.replaceAll("\\s", "%20") }
+    s"${CURL_PATH} http://${localAddress}:${AppConfig.PORT}/ -X POST ${param}"
   }
 
 }
