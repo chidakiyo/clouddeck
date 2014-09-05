1c04718d55af4738bab5fa4a8470b0cb4d7c576f
yukinagae <yuki.nagae1130@gmail.com>
2014-09-05 11:18:22 +0900

    (refs #13)api for guest os power on/off

--- src/main/scala/clouddeck/command/VIXCommandBuilder.scala
+++ src/main/scala/clouddeck/command/VIXCommandBuilder.scala
@@ -5,11 +5,6 @@ import clouddeck.Keys.Const._
 
 object VIXCommandBuilder {
 
-  /** get guest os list */
-  def guestList()(implicit info: ConnectInfo): String = {
-    s"${VIX_CMD.cmdAndOpt_vix(info)} list"
-  }
-
   /** guest os power on */
   def guestPowerOn(path: String)(implicit info: ConnectInfo): String = {
     s"${VIX_CMD.cmdAndOpt_vix(info)} start ${quot(path)}"
@@ -18,16 +13,6 @@ object VIXCommandBuilder {
   /** guest os power off */
   def guestPowerOff(path: String)(implicit info: ConnectInfo): String = {
     s"${VIX_CMD.cmdAndOpt_vix(info)} stop ${quot(path)}"
-  }
-
-  def main(args: Array[String]) {
-    implicit val info = ConnectInfo("192.168.1.1", "root", "pass")
-    val cmd = guestPowerOff("<path>.vmx")
-    println(cmd)
-    import scala.sys.process._
-    val result = cmd.lines_!
-    println(result)
-
   }
 
 }
