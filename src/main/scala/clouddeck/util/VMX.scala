e73e101a9b3e27cb867c711609b4233d3ea1b21b
yukinagae <yuki.nagae1130@gmail.com>
2014-09-17 16:31:48 +0900

    (refs #20)VMwareTools status 3 cases (1. NotInstalled, 2. Installed, 3. Installed Old version)

--- src/main/scala/clouddeck/util/VMX.scala
+++ src/main/scala/clouddeck/util/VMX.scala
@@ -3,8 +3,8 @@ package clouddeck.util
 import spray.json._
 import DefaultJsonProtocol._
 
-case class VMX(name: String, image: String, storage: String, fullPath: String, isOn: Option[Boolean] = None)
+case class VMX(name: String, image: String, storage: String, fullPath: String, isOn: Option[Boolean] = None, vmwareToolsStatus: Option[String] = None)
 
 object VMXJsonProtocol extends DefaultJsonProtocol {
-  implicit val vmxFormat = jsonFormat5(VMX)
+  implicit val vmxFormat = jsonFormat6(VMX)
 }
