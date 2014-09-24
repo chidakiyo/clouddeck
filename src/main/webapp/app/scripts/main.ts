d027f6bd6539784568743f523cbce72fd7387bde
kiyo <kiyokatsu.chida@weserve.co.jp>
2014-09-24 15:35:40 +0900

    refactoring type script

--- src/main/webapp/app/scripts/main.ts
+++ src/main/webapp/app/scripts/main.ts
@@ -6,18 +6,17 @@ module Model {
 	export class EHost {
 		constructor(data:any){
 			this.name(data.name)
-			Net.guest(Keys.URL.guests + this.name(), this.child, Net.chost);
+			Net.guest(this.name(), this.child)
 		}
-		name = ko.observable()
+		name:KnockoutObservable<string> = ko.observable("")
 		child = ko.observableArray()
 		getChildren(){
 			Init.model.svname(this.name())
-			Net.guest(Keys.URL.guests + this.name(), Init.model.clienthosts, Net.chost);
+			Net.guest(this.name(), Init.model.clienthosts)
 		}
 	}
 	export class CHost {
 		constructor(data:any){
-//		constructor(name:string, power:boolean, full:string){
 			this.name(data.name)
 			this.power(data.isOn)
 			this.full(data.full)
@@ -47,23 +46,17 @@ module Keys {
 }
 
 class Net {
-	static host(url:string, list:KnockoutObservableArray<any>, create:any) {
-		$.getJSON(url, function(data:any){
-			list.removeAll();
-			_(data.success.data).each(function(host){
-				list.push(create(host))
-			})
-		})
+	static host(list:KnockoutObservableArray<any>) {
+		list.removeAll()
+		$.getJSON(Keys.URL.hosts)
+		 .done((data:any) => data.success.data.map(host => list.push(Net.ehost(host))))
 	}
-	static guest(url:string, list:KnockoutObservableArray<any>, create:any){
-		$.getJSON(url, function(data:any){
-			list.removeAll();
-			_(data.success.data).each(function(host){
-				list.push(create(host))
-			})
-		})
+	static guest(url:string, list:KnockoutObservableArray<any>){
+		list.removeAll()
+		$.getJSON(Keys.URL.guests + url)
+		 .done((data:any) => data.success.data.map(host => list.push(Net.chost(host))))
 	}
-
+	
 	static ehost = (host:any) => new Model.EHost(host)
 	static chost = (host:any) => new Model.CHost(host)
 }
@@ -73,10 +66,10 @@ class Init {
 	static DEBUG:boolean = false
 	static model:Model.Vmodel
 	constructor(){
-		Init.model = new Model.Vmodel();
-		ko.applyBindings(Init.model);
-		Net.host(Keys.URL.hosts, Init.model.esxhosts, Net.ehost);
+		Init.model = new Model.Vmodel()
+		ko.applyBindings(Init.model)
+		Net.host(Init.model.esxhosts)
 	}
 }
 
-$(() => new Init());
+$(() => new Init())
