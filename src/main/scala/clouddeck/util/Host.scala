ee72c24ec8b90f70914ad0acefaccb6332b7fef4
yukinagae <yuki.nagae1130@gmail.com>
2014-10-02 17:22:34 +0900

    (refs #33)add ESXi host info about "nickname(= just nickname to identify but not actually hostname)" and "description".

--- src/main/scala/clouddeck/util/Host.scala
+++ src/main/scala/clouddeck/util/Host.scala
@@ -3,8 +3,8 @@ package clouddeck.util
 import spray.json._
 import DefaultJsonProtocol._
 
-case class Host(name: String)
+case class Host(name: String, nickname: Option[String] = None, description: Option[String] = None)
 
 object HostJsonProtocol extends DefaultJsonProtocol {
-  implicit val vmxFormat = jsonFormat1(Host)
+  implicit val vmxFormat = jsonFormat3(Host)
 }
